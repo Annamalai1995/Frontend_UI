dffbfdc0612dc0b8a9014f6c98a8aaa116d13a74
Annamalai1995 <s.annamalai1995@gmail.com>
2025-07-21 14:26:41 +0530

    crud completed

--- Javascript/crud.js
+++ Javascript/crud.js
@@ -18,3 +18,49 @@ let  List=()=>
    document.getElementById('list').innerHTML="<table><thead bgcolor='blue'><tr><th>COOLING PLACES</th></tr></thead><tbody bgcolor='violet'align='center'>"+temp+"</tbody></table>";
 
 }
+
+let Edit=()=>
+{
+    let updateindex=document.getElementById('Cool').value;
+    let newvalue=prompt("Tell us update va.ues"+Cooling_places)
+    Cooling_places[updateindex]=newvalue
+    List()
+
+}
+let Remove=()=>
+{
+    var delete_values=document.getElementById('Cool').value;
+    Cooling_places=Cooling_places.filter((getting)=>
+    {
+
+        if(getting!==delete_values)  //ooty!==ooty
+        {
+            return getting
+        }
+    })
+    List()
+
+}
+let search=()=>
+{
+    let search=document.getElementById('Cool').value;
+    for(i=0;i<=Cooling_places.length;i++)
+    {
+        if(Cooling_places[i]===search)
+        {
+            return i;
+        }
+    }
+
+}
+
+let First=()=>
+{
+    let search=document.getElementById('Cool').value;
+    for(i=0;i<=Cooling_places.length;i++)
+    {
+        alert("Cooling Place"+Cooling_places[i]);
+        break;
+    }
+
+}
